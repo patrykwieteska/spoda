4d3113cbc779401affb794af2703e57d1505feb9
wi3tek <patrykwieteskapw@gmail.com>
2022-12-01 11:53:56 +0100

    Initial commit

---
+++ src/main/java/pl/pw/spoda/SpodaApplication.java
@@ -0,0 +1,13 @@
+package pl.pw.spoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pw/spoda/SpodaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pw.spoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
